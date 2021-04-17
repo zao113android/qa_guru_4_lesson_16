ed685f52134945b4b640156f35274ef5e3a1e807
Anna <zykova.anna.zao@gmail.com>
2021-04-17 17:05:01 +0300

    small fixes

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,14 +1,19 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 
 public class TestBase {
 
     @BeforeAll
     static void setUp() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
         RestAssured.baseURI = "http://demowebshop.tricentis.com";
         Configuration.baseUrl = "http://demowebshop.tricentis.com";
     }
