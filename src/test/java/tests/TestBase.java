e61398b0b80436d4f1c37d46dbd8769ece92e628
Anna <zykova.anna.zao@gmail.com>
2021-04-17 19:44:04 +0300

    small fixes

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -44,6 +44,6 @@ public class TestBase {
         attachAsText("Browser console logs", getConsoleLogs());
         if(config.getVideoStorage() != null)
             attachVideo();
-        closeWebDriver();
+       // closeWebDriver();
     }
 }
